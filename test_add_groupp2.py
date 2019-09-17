bcb41ecb0d0fd0d77b03e31a3c3280f355b00176
El <547457457545678@mail.ru>
2019-09-17 19:45:22 +0700

    Создание вспомогательных методов, повышение "читаемости" кода, праметры

--- test_add_groupp2.py
+++ test_add_groupp2.py
@@ -13,33 +13,31 @@ class TestAddGroup2(unittest.TestCase):
     def TestAddGroup2(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="fgjfgj", header="fgjfgj", footer="fgjfgj")
         self.return_to_groups(wd)
         self.logout(wd)
 
     def logout(self, wd):
-        # выход
         wd.find_element_by_link_text("Logout").click()
 
     def return_to_groups(self, wd):
-        # возврат на страницу групп
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # создание новой группы
         wd.find_element_by_name("new").click()
         # заполнение формы
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("fgjfgj")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("fgjfgj")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("fgjfgj")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # сохранение группы
         wd.find_element_by_name("submit").click()
 
@@ -48,14 +46,14 @@ class TestAddGroup2(unittest.TestCase):
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
