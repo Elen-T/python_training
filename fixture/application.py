47820adecea58ab8ce782206045868ac691e11f2
El <547457457545678@mail.ru>
2019-09-25 10:00:44 +0700

    Задание №5: Реализовать двухуровневую архитектуру тестов (контакты)

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -9,37 +10,11 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def return_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
-
-    def create_contact(self, contacts):
-        wd = self.wd
-        self.open_edit_contact_page()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contacts.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contacts.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contacts.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contacts.nickname)
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        self.return_home_page()
-
-    def open_edit_contact_page(self):
-        # открытие страницы добавления контакта
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
 
     def destroy(self):
         self.wd.quit()
--- test/TestAddContact.py
+++ test/TestAddContact.py
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contacts(firstname="esryewry", middlename="weywey", lastname="weywey", nickname="weywye"))
+    app.contact.create(Contacts(firstname="esryewry", middlename="weywey", lastname="weywey", nickname="weywye"))
     app.session.logout()
 
 
