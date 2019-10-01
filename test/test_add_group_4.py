b14641323eeb842f0395e7b3fbf0913903a2ff8a
El <547457457545678@mail.ru>
2019-10-01 10:31:36 +0700

    Задание №8.Повышаем "интеллектуальность" фикстуры: валидация фикстуры перед каждым тестом

--- test/test_add_group_4.py
+++ test/test_add_group_4.py
@@ -4,12 +4,8 @@ from model.group import Group
 
 
 def test_add_groupp2(app):
-    # app.session.login(username="admin", password="secret")
     app.group.create(Group(name="fgjfgj", header="fgjfgj", footer="fgjfgj"))
-    app.session.logout()
 
 
 def test_add_empty_groupp2(app):
-    # app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
