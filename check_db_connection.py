fc2bbaaf7f8d2b8a713cdd694978479b4bec4d0f
El <547457457545678@mail.ru>
2019-11-01 17:30:20 +0700

    Отношения между объектами

--- check_db_connection.py
+++ check_db_connection.py
@@ -3,18 +3,28 @@
 # import pymysql.cursors
 import pymysql
 from fixture.orm import ORMFixture
+from model.group import Group
 
 
 db = ORMFixture(host="127.0.0.1", name = "addressbook", user ="root", password = "") # будет создаваться новый объект типа DbFixture
 #connection = pymysql.connect(host="127.0.0.1", database = "addressbook", user ="root", password = "") # устанавливаем соединение
 
 try: # вывод инфы на консоль
-    l = db.get_contact_list()
+    l = db.get_contacts_not_in_group(Group(id="233"))
     for item in l:
         print(item)
     print(len(l)) #печатаем длинну списка
 finally:
      pass # db.destroy()
+
+
+"""try: # вывод инфы на консоль
+    l = db.get_contacts_in_group(Group(id="230"))
+    for item in l:
+        print(item)
+    print(len(l)) #печатаем длинну списка
+finally:
+     pass # db.destroy()"""
 
 """try: 
     cursor = connection.cursor()  # для создания запроса к бд нужно создать курсор, указатель на данные в бд
